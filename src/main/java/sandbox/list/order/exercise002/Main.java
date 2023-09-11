f7b395c45abb05638be26de8974dd46b58eaabed
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-09-11 20:52:42 -0300

    feat: Exercise002 completed

---
+++ src/main/java/sandbox/list/order/exercise002/Main.java
@@ -0,0 +1,17 @@
+package main.java.sandbox.list.order.exercise002;
+
+public class Main {
+    public static void main(String[] args) {
+        SumNumbers sumNumbers = new SumNumbers();
+
+        sumNumbers.addNumber(1);
+        sumNumbers.addNumber(5);
+        sumNumbers.addNumber(9);
+        sumNumbers.addNumber(12);
+
+        System.out.println("Menor número: " + sumNumbers.findLowestNumber());
+        System.out.println("Maior número: " + sumNumbers.findHighestNumber());
+        System.out.println("Soma: " + sumNumbers.calculateSum());
+        System.out.println("Lista: " + sumNumbers.showNumberList());
+    }
+}
