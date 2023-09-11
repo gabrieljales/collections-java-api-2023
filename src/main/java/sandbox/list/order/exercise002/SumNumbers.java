09c13d9414433f159bb817218176b886486d0b64
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-09-11 20:52:31 -0300

    feat: Create SumNumbers class

---
+++ src/main/java/sandbox/list/order/exercise002/SumNumbers.java
@@ -0,0 +1,54 @@
+package main.java.sandbox.list.order.exercise002;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumNumbers {
+    List<Integer> numberList;
+
+    public SumNumbers() {
+        this.numberList = new ArrayList<>();
+    }
+
+    public void addNumber(int number) {
+        numberList.add(number);
+    }
+
+    public int calculateSum() {
+        int total = 0;
+
+        for (int number : numberList) {
+            total += number;
+        }
+
+        return total;
+    }
+
+    public int findHighestNumber() {
+        int highestNumber = 0;
+
+        for (int number : numberList) {
+            if (number > highestNumber) {
+                highestNumber = number;
+            }
+        }
+
+        return  highestNumber;
+    }
+
+    public int findLowestNumber() {
+        int lowestNumber = 0;
+
+        for (int number : numberList) {
+            if (number < lowestNumber) {
+                lowestNumber = number;
+            }
+        }
+
+        return  lowestNumber;
+    }
+
+    public List<Integer> showNumberList() {
+        return numberList;
+    }
+}
